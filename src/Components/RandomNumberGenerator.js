a7b786f2353d5fbe3416a140ba660204764e6633
David Zombar <davidzombar@gmail.com>
2021-09-05 21:16:02 -0600

    RandomNumberGenerator function completed, return value still needs added

---
+++ src/Components/RandomNumberGenerator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function RandomNumberGenerator() {
+    function randNum(min, max){
+        Math.floor(Math.random()*(max-min +1)+min;
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RandomNumberGenerator
