777783e5e4c9f6950732b2f0d9b19dd8373307be
David Zombar <davidzombar@gmail.com>
2021-09-06 11:38:12 -0600

    saved progress on MagicBall Component

---
+++ src/Components/MagicBall.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MagicBall() {
+  const positiveResponse = ['Yes', 'Looks Likely', 'Absolutely', 'Very Likely'];
+  const negativeResponse = ['No', "Don't Count On It", 'No Way', 'Not Likely'];
+  const neutralResponse = ['Maybe', 'Outlook Unknown', 'Ask Again Later'];
+  return <div></div>;
+}
+
+export default MagicBall;
